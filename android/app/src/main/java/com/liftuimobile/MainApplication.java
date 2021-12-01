5087e4c801dc95b2cf1e9318ff5067ee3b05c804
Daniel Osmond <daniel@thewelift.com>
2021-12-01 15:46:24 -0700

    Removed welcome

--- android/app/src/main/java/com/liftuimobile/MainApplication.java
+++ android/app/src/main/java/com/liftuimobile/MainApplication.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.horcrux.svg.SvgPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactInstanceManager;
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,5 +1,4 @@
 export * from './Button.stories';
-export * from './Welcome.stories';
 export * from './Checkbox.stories';
 export * from './Icon.stories';
 export * from './Tile.stories';
