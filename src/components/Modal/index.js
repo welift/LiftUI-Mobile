f7b1af0daa267d27b4a6cdf0dc2c8b4f108bbd46
tolby-welift <86249165+tolby-welift@users.noreply.github.com>
2021-12-01 17:36:29 -0700

    remove centerview

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -20,23 +20,21 @@ const Modal = ({
   ...rest
 }) => {
   return (
-    <View style={styles.centerView(visible)}>
-      <NativeModal 
-        transparent={false}
-        visible={visible}
-        onRequestClose={onClose}
-        {...rest}
-      >
-        <View style={styles.centerView(visible)}>
-        <Tile padding={20} style={styles.tile}>
-          <Pressable onPress={onClose}><Icon name='closeX' width={16} height={16} /></Pressable>
-            <Text style={[types.headingThree, styles.title]}>{title}</Text>
-              {children}
-            <Button onPress={onButtonPress}>{buttonText}</Button>
-        </Tile>
-        </View>
-      </NativeModal>
-    </View>
+    <NativeModal 
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+      {...rest}
+    >
+      <View style={styles.innerCenter(visible)}>
+      <Tile padding={20} style={styles.tile}>
+        <Pressable onPress={onClose}><Icon name='closeX' width={16} height={16} /></Pressable>
+          <Text style={[types.headingThree, styles.title]}>{title}</Text>
+            {children}
+          <Button onPress={onButtonPress}>{buttonText}</Button>
+      </Tile>
+      </View>
+    </NativeModal>
   )
 }
 
--- src/components/Modal/modalStyles.js
+++ src/components/Modal/modalStyles.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  centerView: (visible) => ({
+  innerCenter: (visible) => ({
     flex: 1,
     display: 'flex',
     justifyContent: "center",
