b3d69c1aa7699f33a268dce025329ea596aeb8fa
Daniel Osmond <daniel@thewelift.com>
2021-12-02 13:29:35 -0700

    Beginning input stuff but also fixed font problem

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -1,15 +1,10 @@
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
-import { View } from 'react-native';
 import Input from '.';
 import CenterView from '../CenterView';
 
 storiesOf('Input', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add('is enabled', () => (
-    <View>
-      <Input placeholder='test' />
-      <Input placeholder='test2' />
-    </View>
-
+  .add('Default', () => (
+    <Input />
   ))
