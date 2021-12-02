e42b281d76df56daa1a4e13c0aaccc0fc4be0473
Daniel Osmond <daniel@thewelift.com>
2021-12-02 09:32:57 -0700

    File rearrange for bob the builder

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Button } from './Button'
 export { default as Checkbox } from './Checkbox'
 export { default as Icon } from './Icon'
+export { default as Tile } from './Tile'
+export { default as Line } from './Line'
