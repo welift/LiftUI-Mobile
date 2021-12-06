5644698b4a52140540cb818b95b15c437b53c19a
Daniel Osmond <daniel@thewelift.com>
2021-12-06 08:58:42 -0700

    Updated styles

---
+++ src/components/Stepper/stepperStyles.js
@@ -0,0 +1,18 @@
+import colors from '@theme/color'
+import { StyleSheet } from 'react-native'
+
+export const stepStyles = StyleSheet.create({
+  defaultStep: {
+    backgroundColor: colors.secondaryLight,
+  },
+  currentStep: {
+    backgroundColor: colors.primary,
+    borderColor: colors.primary,
+    borderWidth: 2,
+  },
+  indicatorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  }
+})
